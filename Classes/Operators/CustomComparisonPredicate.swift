16d22196b4bfc544eada821ae657258df185d9dc
Antonio Posabella <antonioposabella91@gmail.com>
2020-05-31 22:55:13 +0200

    custom infix contains operator

--- Classes/Operators/CustomComparisonPredicate.swift
+++ Classes/Operators/CustomComparisonPredicate.swift
@@ -16,3 +16,13 @@ import Foundation
 public func == <Element: Equatable, Root, Value: KeyPath<Root, Element>>(keyPath: Value, value: Element) -> Comparison<Root> {
 	Comparison(keyPath, .equalTo, value)
 }
+
+infix operator ⊂
+/// Contains operator
+/// - Parameters:
+///   - keyPath: KeyPath<Root, Element>
+///   - value: value to compare
+/// - Returns: Comparison
+public func ⊂ <Element: Equatable, Root, Value: KeyPath<Root, Element>>(keyPath: Value, value: Element) -> Comparison<Root> {
+	Comparison(keyPath, .contains, value)
+}
