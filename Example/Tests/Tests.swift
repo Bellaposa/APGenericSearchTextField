4910ef6f71be6fe770877949529c13ed886bc79f
Antonio Posabella <>
2020-06-23 21:15:49 +0200

    Update Tests.swift

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -4,13 +4,14 @@ import APGenericSearchTextField
 class Tests: XCTestCase {
 
 	private var persons: [Person] = []
-	private var textField: GenericSearchTextField<Any>?
+	private var textField: GenericSearchTextField<Person, PersonTableViewCell>?
 
 	override func setUp() {
 		super.setUp()
 		persons = [Person(name: "LeBron", surname: "James"), Person(name: "Larry", surname: "Bird")]
 		let frame = CGRect(x: 150, y: 150, width: 200, height: 20)
-		textField = GenericSearchTextField(model: persons, frame: frame, cellConfigurator: { (_, _) in
+		textField = GenericSearchTextField(model: persons, frame: frame, cellConfigurator: { (person, cell) in
+			return cell
 		})
 	}
 
@@ -39,5 +40,12 @@ private class Person: NSObject {
 	init(name: String, surname: String) {
 		self.name = name
 		self.surname = surname
+	}
+}
+
+private class PersonTableViewCell: BaseTableViewCell<Person> {
+
+	override func awakeFromNib() {
+		super.awakeFromNib()
 	}
 }
